5613d9271bbcba89936f3d600d9491d0708c0643
Marcos <markintinky@gmail.com>
2025-04-04 22:29:53 -0300

    chore:v0.1.4

--- confirmation_dialog/__init__.py
+++ confirmation_dialog/__init__.py
@@ -2,7 +2,7 @@
 Confirmation Dialog - A simple Python package for creating confirmation dialogs.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .confirmation_dialog import ConfirmationDialog
 
